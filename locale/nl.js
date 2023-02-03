4c41542a4384291cae4a631259363eb8551c3b80
Lars van de Donk <l.vandedonk@gmail.com>
2023-02-03 09:37:54 +0100

    Add Dutch translations

---
+++ locale/nl.js
@@ -0,0 +1,15 @@
+export default {
+  barTitle: 'Cookies',
+  barDescription: 'We gebruiken eigen cookies en cookies van derden om je deze website te tonen en om beter te begrijpen hoe jij deze gebruikt, met als doel onze diensten te verbeteren. Als je doorgaat op deze website beschouwen we dit als acceptatie van de cookies.',
+  acceptAll: 'Alles accepteren',
+  declineAll: 'Alles afwijzen',
+  manageCookies: 'Beheer cookies',
+  unsaved: 'Je hebt niet-opgeslagen instellingen',
+  close: 'Sluiten',
+  save: 'Opslaan',
+  necessary: 'Benodigde cookies',
+  optional: 'Optionele cookies',
+  functional: 'Functionele cookies',
+  blockedIframe: 'Schakel functionele cookies in om dit te zien',
+  here: 'hier'
+}
